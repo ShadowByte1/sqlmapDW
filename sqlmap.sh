71cce413e4f2e5ec4ebda1def6aa173f8c08b1b6
ShadowByte <155693555+ShadowByte1@users.noreply.github.com>
2024-07-29 16:38:52 +1000

    Update sqlmap.sh
    Updated Script to allow STDIN using the waybackurls command and so forth

--- sqlmap.sh
+++ sqlmap.sh
@@ -11,20 +11,32 @@ send_alert() {
   curl -H "Content-Type: application/json" -X POST -d "$payload" "$webhook_url"
 }
 
+# Function to process URLs
+process_urls() {
+  while IFS= read -r url; do
+    echo "Testing URL: $url"
+    sqlmap -u "$url" "$@" --batch --hostname --alert="./webhook.sh '$url'"
+  done
+}
+
 # Check if -f or -u option is provided
 if [ "$1" == "-f" ]; then
   file=$2
   shift 2
-  while IFS= read -r url; do
-    echo "Testing URL: $url"
-    sqlmap -u "$url" "$@" --batch --hostname --alert="./webhook.sh '$url'"
-  done < "$file"
+  if [ -f "$file" ]; then
+    cat "$file" | process_urls
+  else
+    echo "File not found: $file"
+    exit 1
+  fi
 elif [ "$1" == "-u" ]; then
   url=$2
   shift 2
   echo "Testing URL: $url"
   sqlmap -u "$url" "$@" --batch --hostname --alert="./webhook.sh '$url'"
-else
-  echo "Usage: $0 -f <file> [options] | -u <url> [options]"
+elif [ -t 0 ]; then
+  echo "Usage: $0 -f <file> [options] | -u <url> [options] | <urls from stdin>"
   exit 1
+else
+  process_urls "$@"
 fi
